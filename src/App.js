5587868018367b91ef45209d9e28dbd3c4b2efba
Ayon <ayonjodder177@gmail.com>
2022-06-11 04:32:41 +0600

    Route updated

--- src/App.js
+++ src/App.js
@@ -13,9 +13,7 @@ function App() {
     <div>
       <Navbar />
       <Routes>
-        <Route path='/' element={<RequiredAuth>
-          <Home />
-        </RequiredAuth>}></Route>
+        <Route path='/' element={<Home />}></Route>
         <Route path='login' element={<Login />}></Route>
         <Route path='signup' element={<SignUp />}></Route>
         <Route path='create-candidate' element={
